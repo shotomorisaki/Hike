1f57202a7e748f1badfc69ac2d8f8a8619483768
Mustafa Tan <60370747+atakan682@users.noreply.github.com>
2022-05-27 11:09:22 -0700

    Update MemberList.h

--- MemberList.h
+++ MemberList.h
@@ -1,10 +1,10 @@
+//fixed errors(atakan)
+
 /*
     Team Morisaki
-
     Claude, Austin // member 1
     Morisaki, Shoto // member 2
     Mustafa Atakan Tan // member 3
-
     Spring 2022
     CS A250 - C++ 2
     Project: Hiking in the US
@@ -12,22 +12,28 @@
 
 #ifndef MEMBERLIST_H
 #define MEMBERLIST_H
-#include "Hike.h"
+
 #include "Member.h"
+
 #include <string>
-#include <map>
 #include <set>
-//You were missing global constant 
-const int MEMBER_ID = 111;
-class MemberList : public Member
+
+
+const int MEMBER_ID = 111;//global constant storing first ID 
+
+class MemberList
 {
 public:
     MemberList();
+
     void addMember(const std::string& firstName, const std::string& lastName);
-    void addMember(std::string firstName, std::string lastName, int points);
+    void addMember(const std::string firstName, const std::string lastName, int points);
+
     int getLastID();
     int getPoints(int memberID);
-    void printMember(int memberID, std::string lastName);
+
+    void printMember(int memberID, const std::string lastName);
+
     void clearList();
     ~MemberList();
 
@@ -36,4 +42,3 @@ private:
     std::set<Member>* ptrToSet;
 };
 #endif
-
