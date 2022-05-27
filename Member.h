7cf9a5d2cbca5be96366c67d6995430a1711fea4
Mustafa Tan <60370747+atakan682@users.noreply.github.com>
2022-05-27 11:06:26 -0700

    Update Member.h

--- Member.h
+++ Member.h
@@ -1,10 +1,8 @@
 /*
     Team Morisaki
-
     Claude, Austin // member 1
     Morisaki, Shoto // member 2
     Mustafa Atakan Tan // member 3
-
     Spring 2022
     CS A250 - C++ 2
     Project: Hiking in the US
@@ -12,27 +10,36 @@
 
 #ifndef MEMBER_H
 #define MEMBER_H
-#include <string>
 
+#include <string>
 
 class Member
 {
 public:
-    Member();
-    Member(std::string fName, std::string lName);
-    bool operator<(const Member& rightMember)const;
+
+    Member(int memberID = 0, int points = 0) {};//default constructor
+    Member(const std::string fName, const std::string lName); //overloaded constructor
+
+
     void addPoints(int val);
     void setID(int id);
+
     int getMemberID() const;
     std::string getLastName() const;
     int getPoints() const;
+
+    bool operator<(const Member& rightMember)const;
+
     void print() const;
+
+    Member();//destructor
 
 private:
     int memberID = 0;
+    int points = 0;
+
     std::string firstName;
     std::string lastName;
-    int points = 0;
+    
 };
-
 #endif
