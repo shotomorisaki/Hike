e87db940f4f01d65b440592ac4e51ea2ec1e2797
shotomorisaki <103382369+shotomorisaki@users.noreply.github.com>
2022-05-28 15:01:17 -0700

    Update Interface.cpp

--- Interface.cpp
+++ Interface.cpp
@@ -28,36 +28,34 @@ void Interface::displayMenu() {
     cout << "\t 5. Make a reservation " << endl;
     cout << "\t 6. View reservation" << endl;
     cout << "\t 7. Cancel reservation" << endl;
-    cout << "\t 8. Exit" << endl;
+    cout << "\t 8. Exit" << endl << endl;
 }
 //This is signed by Austin part
 void Interface::processReservation(HikeList& Hlist, MemberList& MList, Reservations& reservations) {
     cout << "Please make a selection: ";
     cin >> userSelection;
-    while (userSelection != 8) {
-        if (userSelection == 1)
-            chooseByLocation(Hlist, MList, reservations);
-        else if (userSelection == 2)
-            chooseByDuration(Hlist, MList, reservations);
-        else if (userSelection == 3)
-            chooseByDifficulty(Hlist, MList, reservations);
-        else if (userSelection == 4)
-            chooseByPrice(Hlist, MList, reservations);
-        else if (userSelection == 5)
-            makeReservation(Hlist, MList, reservations);
-        else if (userSelection == 6)
-            viewReservation(Hlist, MList, reservations);
-        else if (userSelection == 7)
-            cancelReservation(Hlist, MList, reservations);
-    }
-    if (userSelection == 8) {
+    if (userSelection == 1)
+        chooseByLocation(Hlist, MList, reservations);
+    else if (userSelection == 2)
+        chooseByDuration(Hlist, MList, reservations);
+    else if (userSelection == 3)
+        chooseByDifficulty(Hlist, MList, reservations);
+    else if (userSelection == 4)
+        chooseByPrice(Hlist, MList, reservations);
+    else if (userSelection == 5)
+        makeReservation(Hlist, MList, reservations);
+    else if (userSelection == 6)
+        viewReservation(Hlist, MList, reservations);
+    else if (userSelection == 7)
+        cancelReservation(Hlist, MList, reservations);
+    else if (userSelection == 8) 
         cout << "Thank you for visiting!";
-    }
 }
 //This is signed by Austin part
 void Interface::chooseByLocation(HikeList& Hlist, MemberList& MList, Reservations& reservations) {
     //you cant call a function using HikeList::printbyprice() need a HikeList obj like so..
     Hlist.printAllLocations();
+    cout << endl;
     cout << "Choose a location: ";
     cin >> chooseLocation;
     //you cant call a function using HikeList::printbyprice() need a HikeList obj like so..
@@ -90,14 +88,11 @@ void Interface::chooseByDifficulty(HikeList& HList, MemberList& MList, Reservati
 
 void Interface::chooseByPrice(HikeList& HList, MemberList& MList, Reservations& reservations)
 {
-    //edited
+    //you cant call a function using HikeList::printbyprice() need a HikeList obj like so..
     HList.printByPrice();
-    cout << endl;
- 
-    cout << "Would you like to make a reservation? (y/n) ";
-    cin >> reservationsOrNot;
-    
-    cout << endl;
+    cout << "Would you like to reserve? (y/n)" << endl;
+    //you cant cin an obj of type reservation the compiler does not know how to do this.
+    cin >> userSelection;
     askToReserve(HList, MList, reservations);
 }
 
@@ -148,55 +143,61 @@ void Interface::cancelReservation(HikeList& HList, MemberList& MList, Reservatio
 
 void Interface::makeReservation(HikeList& HList, MemberList& MList, Reservations& Reserve)
 {
-    int memberID = askIfMember(MList);
+    askIfMember(MList);
+    cout << "Which hike would you like to reserve (hike name)? ";
+    string n;
+    cin >> n;
 
-    cout << "Please make a selection: " << endl;
-    string location;
-    cin >> location;
-
-    HList.printByHikeName(location);
-    HList.getPrice(location);
-    MList.getPoints(memberID);
-    Reserve.addReservation(MList.getMemberID(), location);
+    HList.printByHikeName(n);
+    HList.getPrice(n);
+    MList.getPoints(MList.getMemberID());
+    // cout << "\tDiscounted price using points: " << price << endl;
+    // Reserve.printReservation(MList.getMemberID(), HList, MList);
+    cout << endl;
+    
+    Reserve.addReservation(MList.getMemberID(), n);
+    cout << "\tBefore processing, please make a note of your reservation number. " << endl;
+    cout << "\t Reservation #: " << MList.getMemberID() << endl;
 }
+
+/*
+Ask user which hike to reserve.
+Call function HikeList::printByHikeName() to print the hike chosen by the user.
+Call function HikeList::getPrice() and MemberList::getPoints() to print the discounted price, formatted with two decimal places.
+Call function Reservations::addReservation() to add the reservation to the list.
+*/
 
 void Interface::askToReserve(HikeList& HList, MemberList& MList, Reservations& Reserve)
 {
-    //edited
-    if (reservationsOrNot == 'y')
+    char c;
+    cout << "Would you like to make a reservation? (y/n) ";
+    cin >> c;
+    cout << endl;
+    
+    if (c == 'y')
     {
         makeReservation(HList, MList, Reserve);
     }
-    else if (reservationsOrNot == 'n')
-    {
-        cout << "Press any key to continue . . .";
-        
-
-    }
-    //cout << "Which hike would you like to reserve (hike name)?"
 }
 
 int Interface::askIfMember(MemberList& MList)
 {
-    //edited
-     cout << "Are you a member? (y/n) ";
+    cout << "Are you a member? (y/n) ";
     cin >> memberOrNot;
     cout << endl;
-    if (memberOrNot == 'y') {
-        cout << "What is your Member ID number? " << endl;
-        cin >> memberID;
+    
+    cout << "What is your Member ID number? ";
+    cin >> memberID;
+    cout << endl;
+    
+    cout << "What is your last name? ";
+    cin >> lName;
 
-        cout << "What is your last name? " << endl;
-        cin >> lName;
+    if (memberOrNot == 'y')
         MList.printMember(memberID, lName);
-        return memberID;
-    }
-
-    else {
-
-        return addNewMember(MList);
-    }
- 
+    else
+        addNewMember(MList);
+    return memberID;
 }
 
 
