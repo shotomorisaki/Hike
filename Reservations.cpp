dbae143ad043e04a63406bf9ae663afd21447407
shotomorisaki <103382369+shotomorisaki@users.noreply.github.com>
2022-05-29 09:57:11 -0700

    Update Reservations.cpp

--- Reservations.cpp
+++ Reservations.cpp
@@ -1,17 +1,15 @@
-#include "HikeList.h"
+	#include "HikeList.h"
 #include "MemberList.h"
 #include "Reservations.h"
 #include <string>
 #include <iostream>
 using namespace std;
-
 Reservations::Reservations()
 {
     int count = 0;
     first = nullptr;
     last = nullptr;
 }
-
 int Reservations::addReservation(int memberID, string hikeName)
 {
     if (first == nullptr)
@@ -32,7 +30,6 @@ int Reservations::addReservation(int memberID, string hikeName)
         return newNode->getReservationNumber();
     }
 }
-
 Node* Reservations::findReservation(int reservNum)
 {
     if (first->getReservationNumber() == reservNum)
@@ -51,9 +48,7 @@ Node* Reservations::findReservation(int reservNum)
         }
         return temp;
     }
-
 }
-
 void Reservations::cancelReservation(int reservation_number)
 {
     if (count == 1) {
@@ -71,90 +66,6 @@ void Reservations::cancelReservation(int reservation_number)
         temp = nullptr;
     }
 }
-
-void Reservations::printReservation(int reservNum, HikeList newHikeList, MemberList newMemberList)
-{
-    Node* ptr = findReservation(reservNum);
-    string hikeName = ptr->getHikeName();
-    newHikeList.printByHikeName(hikeName);
-    cout << endl;
-    int memberID = ptr->getMemberID();
-    int points = newMemberList.getPoints(memberID);
-    double price = newHikeList.getPrice(hikeName);
-    price = price - (points / 100);#include "HikeList.h"
-#include "MemberList.h"
-#include "Reservations.h"
-#include <string>
-#include <iostream>
-using namespace std;
-
-Reservations::Reservations()
-{
-    int count = 0;
-    first = nullptr;
-    last = nullptr;
-}
-
-int Reservations::addReservation(int memberID, string hikeName)
-{
-    if (first == nullptr)
-    {
-        last = new Node(RESERVATION_NUMBER, memberID, hikeName, nullptr, nullptr);
-        first = last;
-        ++count;
-        return RESERVATION_NUMBER;
-    }
-    else
-    {
-        Node* newNode = new Node();
-        newNode->setData(newNode->getReservationNumber() + RESERVATION_NUMBER + count, memberID, hikeName);
-        last->setNext(newNode);
-        last = last->getNext();
-        last->setPrev(last->getPrev());
-        ++count;
-        return newNode->getReservationNumber();
-    }
-}
-
-Node* Reservations::findReservation(int reservNum)
-{
-    if (first->getReservationNumber() == reservNum)
-        return first;
-    else if (last->getReservationNumber() == reservNum)
-        return last;
-    else
-    {
-        Node* temp = first->getNext();
-        for (int i = 0; i < count; i++)
-        {
-            if (temp->getReservationNumber() == reservNum)
-                return temp;
-            else
-                temp = temp->getNext();
-        }
-        return temp;
-    }
-
-}
-
-void Reservations::cancelReservation(int reservation_number)
-{
-    if (count == 1) {
-        delete first;
-        delete last;
-        last = nullptr;
-        first = nullptr;
-    }
-    else {
-        Node* temp;
-        temp = findReservation(reservation_number);
-        temp->getPrev()->setNext(temp->getNext());
-        temp->getNext()->setPrev(temp->getPrev());
-        delete temp;
-        temp = nullptr;
-    }
-}
-
 void Reservations::printReservation(int reservNum, HikeList newHikeList, MemberList newMemberList)
 {
     Node* ptr = findReservation(reservNum);
@@ -166,12 +77,4 @@ void Reservations::printReservation(int reservNum, HikeList newHikeList, MemberL
     double price = newHikeList.getPrice(hikeName);
     price = price - (points / 100);
     cout << "\tDiscounted price using points: " << price << endl;
-
-
-
-}
-    cout << "Discounted price using points: " << price << endl;
-
-
-
 }
